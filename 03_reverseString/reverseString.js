016cb4330774087ebe8863e121d8d1f1f9b9eb53
Bernardo <76459310+bernardo-mcosta@users.noreply.github.com>
2022-12-02 16:48:42 -0300

    Add solution to exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let split =  string.split("");
+    let reverse = split.reverse();
+    return reverse.join("")
 };
+
+console.log(reverseString('teste'))
 
 // Do not edit below this line
 module.exports = reverseString;
