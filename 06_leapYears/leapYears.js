c26a3bd6fba8e70605b062d8caef5ada560dc518
Bernardo <76459310+bernardo-mcosta@users.noreply.github.com>
2022-12-02 17:10:47 -0300

    Add solution to exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    return (year % 4 == 0 && (year % 100 !== 0 || year % 400 == 0))
 };
 
+
+console.log(leapYears(2000))
 // Do not edit below this line
 module.exports = leapYears;
+
+// is divisible by 4?
+// is not divisible by 100?
+// is divisible by 400
