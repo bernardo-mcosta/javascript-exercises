bfe0a8265c8f9528c713ad78365bb5219978c73b
Bernardo <76459310+bernardo-mcosta@users.noreply.github.com>
2022-12-02 16:49:05 -0300

    Add solution to exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let sum = 0;
+    if (typeof(a)!= 'number' || typeof(b)!= 'number') return 'ERROR';
+    if (a < 0 || b < 0) return 'ERROR';
+    min = Math.min(a,b);
+    max = Math.max(a,b);
+    for (i = min; i <= max; i++){
+        sum += i ;
+    }
+    return sum;
 };
 
 // Do not edit below this line
